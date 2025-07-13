8e5c81bb09b59d0544e178704251a159cc3c0cc8
Krishil-Jayswal <jayswalkrishil@gmail.com>
2025-07-14 05:03:55 +0530

    feat: implemented pushing cleaned logs to kafka

--- apps/logs-transformer/src/transformers/log-transformer.ts
+++ apps/logs-transformer/src/transformers/log-transformer.ts
@@ -1,4 +1,4 @@
-import { createConsumer } from "@repo/kafka";
+import { createConsumer, producer } from "@repo/kafka";
 import { GroupId, Topic } from "@repo/kafka/meta";
 import { Log } from "@repo/validation";
 import { html_transform } from "./html-transformer.js";
@@ -10,7 +10,6 @@ export class LogTransformer {
     await consumer.subscribe({ topic: Topic.RAW_LOGS });
     consumer.run({
       eachMessage: async ({ message }) => {
-        console.log({ offset: message.offset });
         const log: Log = JSON.parse(message.value?.toString() || "{}");
         log.content = await html_transform(log.content, log.url, {
           onlyMainContent: true,
@@ -18,7 +17,10 @@ export class LogTransformer {
           excludeTags: [],
         });
         log.content = await markdown_transform(log.content);
-        console.log(log);
+        await producer.send({
+          topic: Topic.CLEANED_LOGS,
+          messages: [{ value: JSON.stringify(log) }],
+        });
       },
     });
   }
